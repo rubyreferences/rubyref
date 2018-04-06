4a3eed20654fb8d4940f4574c75dd89825abd5fe
zverok <zverok.offline@gmail.com>
2018-04-06 13:16:40 +0300

    Update list.rb

--- _src/exe/list.rb
+++ _src/exe/list.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
 require 'yaml'
-known = File.read('config/structure.yml').scan(/source: "?(\S+\.md)/).flatten
-Dir['intermediate/parsed/**/*.md'].sort
-  .map { |p| p.sub('intermediate/parsed/', '') }
+known = File.read(File.expand_path('../config/structure.yml', __dir__)).scan(/source: "?(\S+\.md)/).flatten
+Dir[File.expand_path('../intermediate/parsed/**/*.md', __dir__)].sort
+  .map { |p| p.sub(%r{^.+intermediate/parsed/}, '') }
   .yield_self { |l| l - known }
   .map { |p| {'source' => p} }
-  .tap { |l| File.write 'intermediate/list.yml', l.to_yaml }
+  .tap { |l| puts l.to_yaml }
